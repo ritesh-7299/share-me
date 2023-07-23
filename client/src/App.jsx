27830a09113de1d476fd74e1243d769cee589eba
ritesh macwan <riteshmacwan07@gmail.com>
2023-07-24 00:21:32 +0530

    Added some logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import React from 'react'
+import React from "react";
 import { LandingPage } from "./components/LandingPage";
 import { MeetingPage } from "./components/MeetingPage";
 
@@ -7,7 +7,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/meeting/:roomId" element={<MeetingPage />} />
+        {/* <Route path="/meeting/:roomId" element={<MeetingPage />} /> */}
+        <Route path="/meeting" element={<MeetingPage />} />
         <Route path="*" element={<LandingPage />} />
       </Routes>
     </Router>
--- client/src/components/MeetingPage.jsx
+++ client/src/components/MeetingPage.jsx
@@ -1,122 +1,66 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
-import socketIO from 'socket.io-client';
-import { Button, Grid, Typography } from "@mui/material"
+import { Button, Typography } from "@mui/material";
 import { CentralizedCard } from "./CentralizedCard";
-import { Video } from "./Video";
+import { useEffect, useState } from "react";
+import socketIO from "socket.io-client";
+
+const createOffer = async () => {
+  const localConnection = new RTCPeerConnection();
+  localConnection.onicecandidate = (e) => {
+    console.log(" NEW ice candidate!! on localconnection reprinting SDP ");
+    console.log(JSON.stringify(localConnection.localDescription));
+    localStorage.setItem(
+      "signalId",
+      JSON.stringify(localConnection.localDescription)
+    );
+  };
+
+  const sendChannel = localConnection.createDataChannel("sendChannel");
+  sendChannel.onmessage = (e) => console.log("messsage received!!!" + e.data);
+  sendChannel.onopen = (e) => console.log("open!!!!");
+  sendChannel.onclose = (e) => console.log("closed!!!!!!");
+
+  localConnection
+    .createOffer()
+    .then((o) => localConnection.setLocalDescription(o));
+
+  return JSON.stringify(localConnection.localDescription);
+};
 
 export function MeetingPage() {
-    const [socket, setSocket] = useState(null);
-    const [meetingJoined, setMeetingJoined] = useState(false);
-    const [videoStream, setVideoStream] = useState();
-    const [remoteVideoStream, setRemoteVideoStream] = useState();
-    
-    const params = useParams();
-    const roomId = params.roomId;
+  const s = socketIO.connect("http://localhost:3000");
+  const [openConnection, setOpenConnection] = useState(false);
+  const [signalId, setSignalId] = useState(null);
+  const [socket, setSocket] = useState(null);
 
-    useEffect(() => {
-        const s = socketIO.connect('http://localhost:3000');
-        s.on("connect", () => {
-            setSocket(s);
-            s.emit("join", {
-                roomId
-            })
+  const handleSend = async () => {
+    const offer = await createOffer();
+    setSignalId(offer);
+    localStorage.setItem("signalId", offer);
+    setOpenConnection(true);
+  };
+  useEffect(() => {
+    console.log("socket", socket);
+  }, [socket]);
 
-            window.navigator.mediaDevices.getUserMedia({
-                video: true
-            }).then(async (stream) => {
-                setVideoStream(stream);
-            });
-    
-            s.on("localDescription", async ({description}) => {
-                // Receiving video - 
-                let pc = new RTCPeerConnection({
-                    iceServers: [
-                    {
-                        urls: "stun:stun.l.google.com:19302",
-                    },
-                    ],
-                });
-                
-                pc.setRemoteDescription(description);
-                pc.ontrack = (e) => {
-                    setRemoteVideoStream(new MediaStream([e.track]))
-                }
-
-                s.on("iceCandidate", ({candidate}) => {
-                    pc.addIceCandidate(candidate)
-                });
-
-                pc.onicecandidate = ({candidate}) => {
-                    s.emit("iceCandidateReply", {candidate});
-                }
-
-                await pc.setLocalDescription(await pc.createAnswer());
-                s.emit("remoteDescription", {description: pc.localDescription});
-            })
-        });
-    }, [])
-
-    if (!videoStream) {
-        return <div>
-            Loading...
+  return (
+    <div style={{ minHeight: "100vh" }}>
+      <CentralizedCard>
+        <div>
+          <Typography textAlign={"center"} variant="h5">
+            Hi welcome to share-me
+          </Typography>
+          <Typography>{signalId ? "signalId" : "No signal found!"}</Typography>
         </div>
-    }
-
-    if (!meetingJoined) {
-        return <div style={{minHeight: "100vh",}}>
-            <CentralizedCard>
-                <div>
-                    <Typography textAlign={"center"} variant="h5">
-                        Hi welcome to meeting {roomId}.
-                    </Typography>
-                </div>
-                <br/><br/>
-                <div style={{display: "flex", justifyContent: "center"}}>
-                    <Button onClick={() => {
-                        // sending pc
-                        let pc = new RTCPeerConnection({
-                            iceServers: [
-                              {
-                                urls: "stun:stun.l.google.com:19302",
-                              },
-                            ],
-                          });
-                        pc.onicecandidate = ({candidate}) => {
-                            socket.emit("iceCandidate", {candidate});
-                        }
-                        pc.addTrack(videoStream.getVideoTracks()[0])
-                        pc.onnegotiationneeded = async () => {
-                            try {
-                                await pc.setLocalDescription(await pc.createOffer());
-                                console.log(pc.localDescription);
-                                socket.emit("localDescription", {description: pc.localDescription});
-                            } catch (err) {
-                                console.error(err);
-                            }
-                        };
-            
-                        socket.on("remoteDescription", async ({description}) => {
-                            await pc.setRemoteDescription(description);  
-                        });
-                        socket.on("iceCandidateReply", ({candidate}) => {
-                            pc.addIceCandidate(candidate)
-                        });
-                        setMeetingJoined(true);
-                    }} disabled={!socket} variant="contained">
-                        Join meeting
-                    </Button>
-                </div>
-            </CentralizedCard>
+        <br />
+        <br />
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          {!openConnection && (
+            <Button variant="contained" onClick={handleSend}>
+              Send File
+            </Button>
+          )}
         </div>
-    }
-
-    return <Grid container spacing={2} alignContent={"center"} justifyContent={"center"}>
-        <Grid item xs={12} md={6} lg={4}>
-            <Video stream={videoStream} />
-        </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-            <Video stream={remoteVideoStream} />
-        </Grid>
-    </Grid>
+      </CentralizedCard>
+    </div>
+  );
 }
